a7f13183793cc374e5ffc04708ba1ae39ad53946
Alvaro <alvaroluque.g@gmail.com>
2025-09-15 11:21:20 +0200

    actualizacion de comentarios

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,3 @@
-//const mysql = require('mysql2');
 require('dotenv').config();
 const {Sequelize} = require('sequelize');
 
@@ -11,23 +10,3 @@ const sequelize = new Sequelize( // crear instancia de Sequelize
   }
 )
 module.exports = sequelize;
-/*
-// crear la conexión a la bd
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
-// conectar a la bd
-connection.connect((err) => {
-  if (err) {
-    throw err;
-  }else{
-    console.log('Conexión a db correcta');
-  }    
-  })
-
-  module.exports = connection;
-  */
